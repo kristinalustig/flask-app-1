70ccffb221c78bb9a9a2a2ca0cb141f06fc9a4a4
kristinalustig <klustig@rem-klustig-mbp.fios-router.home>
2020-03-24 13:12:30 -0400

    Created empty front-end with basic jquery hover interaction

--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,11 @@
 // Your code starts here
+$(document).ready(function() {
+
+    $(".pokemon")
+        .mouseenter(function() {
+            $(this).css("background-color", "lightblue");
+        })
+        .mouseleave(function() {
+            $(this).css("background-color", "white");
+        })
+});
