3aaf72d3c90314f52314b2dcd1a8fe5bde188fc9
kristinalustig <klustig@rem-klustig-mbp.fios-router.home>
2020-03-25 10:06:11 -0400

    Added in actual pokemon data to index.html

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,30 @@
 // Your code starts here
 $(document).ready(function() {
 
-    $(".pokemon")
-        .mouseenter(function() {
-            $(this).css("background-color", "lightblue");
-        })
-        .mouseleave(function() {
-            $(this).css("background-color", "white");
-        })
-});
+    //loop through and grab all the data, putting them in an array
+    //use the array (not ajax) to create the elements
+
+    $.ajax("api/pokemon",
+        {
+            dataType: 'json',
+            success: function(data) {
+                console.log("success");
+                for (var pokenum = 1; pokenum <= 151; pokenum++) {
+                    $(".pokemon-flex").append(
+                        `<div class="pokemon">
+                            <h2>${data[pokenum-1].name}</h2>
+                            <img class = "pokeimage" src="${data[pokenum-1].image_url}" />
+                        </div>`
+                    );
+                }
+                $(".pokemon")
+                    .mouseenter(function() {
+                        $(this).css("background-color", "lightblue");
+                    })
+                    .mouseleave(function() {
+                        $(this).css("background-color", "white");
+                    })
+            }
+        });
+    });
+    
