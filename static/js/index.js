a7c691e861c8e9fa9a2469dbe5edef1350978a32
kristinalustig <klustig@rem-klustig-mbp.fios-router.home>
2020-03-25 16:23:17 -0400

    Additional cleanup of the toggle function and removal of unnecessary CSS and jQuery

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,6 @@
 // Your code starts here
 $(document).ready(function() {
 
-    //loop through and grab all the data, putting them in an array
-    //use the array (not ajax) to create the elements
-
     var pokemonData;
 
     $.ajax("api/pokemon",
@@ -17,7 +14,7 @@ $(document).ready(function() {
 
                     $(".pokemon-flex").append(
                         `<div class="pokemon" id="${pokemonData[pokenum-1].id}">
-                            <h2 class="pokename">${pokemonData[pokenum-1].id}</h2>
+                            <h2 class="pokename">${pokemonData[pokenum-1].name}</h2>
                             <img class = "pokeimage" src="${data[pokenum-1].image_url}" />
                         </div>`
                     );
@@ -31,129 +28,118 @@ $(document).ready(function() {
                     .click(navigateToEvolution);
 
             }
-        });
+    });
+
+    $(".modal")
+        .click(toggleDetails);
 
 
-        function pokehoverEnter() {
-            $(this).css("background-color", "lightblue");
-        }
+//click handlers
+    function pokehoverEnter() {
+        $(this).css("background-color", "lightblue");
+    }
 
-        function pokehoverLeave() {
-            $(this).css("background-color", "white");
-        }
-
+    function pokehoverLeave() {
+        $(this).css("background-color", "white");
+    }
 
 //brings you from the evolution you click on to the relevant other popup.
-        function navigateToEvolution(event) {
+    function navigateToEvolution(event) {
 
-            //don't trigger the parent click
-            event.stopPropagation();
+        //don't trigger the parent click
+        event.stopPropagation();
 
-            //identify the destination
-            var navigationTarget = $(this).text();
-            console.log("clicked " + navigationTarget);
+        //identify the destination
+        var navigationTarget = $(this).text();
+        console.log("clicked " + navigationTarget);
 
-            //close the current window
-            console.log("close " + $(this).closest(".pokemon").find(".pokename").text());
-            toggleDetails($(this).closest(".pokemon"));
-            //$(this).closest(".pokemon").toggleDetails;
+        //close the current window
+        console.log("close " + $(this).closest(".pokemon").find(".pokename").text());
+        toggleDetails($(this).closest(".pokemon"));
+        //$(this).closest(".pokemon").toggleDetails;
 
-            //open the new window
-            //$(document.getElementById(navigationTarget)).toggleDetails;
-        }
+        //open the new window
+        //$(document.getElementById(navigationTarget)).toggleDetails;
+    }
 
 //helper function - given an ID, return that pokemon's data
-        function pokeHelper(id) {
+    function pokeHelper(id) {
 
-            console.log(pokemonData[id-1]);
+        return pokemonData[id-1];
 
-            return pokemonData[id-1];
-
-        }
+    }
 
 //takes an array of types and outputs a string of formatted list items to append
-        function listMaker(typeArray) {
-            var typeString = "";
-            for (var i = 0; i < typeArray.length; i++) {
-                typeString += `<li>${typeArray[i]}</li>
-                `;
-            }
-            return typeString;
+    function listMaker(typeArray) {
+        var typeString = "";
+        for (var i = 0; i < typeArray.length; i++) {
+            typeString += `<li class="type-item">${typeArray[i]}</li>
+            `;
         }
+        return typeString;
+    }
 
 //takes an array of evolution information and returns rows to be inserted into a table
-        function tableRowMaker(evolutionArray) {
-            var tableString = "";
+    function tableRowMaker(evolutionArray) {
+        var tableString = "";
 
-            for (var x = 0; x < evolutionArray.length; x++) {
-                tableString += 
-                    `<tr>
-                        <td class="evolution-link">${evolutionArray[x].to}</td>
-                        <td>${evolutionArray[x].level}</td>
-                        <td>${evolutionArray[x].method}</td>
-                    </tr>
-                    `;
-            }
-
-            return tableString;
-
+        for (var x = 0; x < evolutionArray.length; x++) {
+            tableString += 
+                `<tr class="evolution-item">
+                    <td class="evolution-link">${evolutionArray[x].to}</td>
+                    <td>${evolutionArray[x].level}</td>
+                    <td>${evolutionArray[x].method}</td>
+                </tr>
+                `;
         }
 
-//Creates a large modal for all pokemon details, or minimizes it.
-        function toggleDetails() {
+        return tableString;
 
-            console.log($(this).attr("id"));
-            var currentPokemon = pokeHelper($(this).attr("id"));
+    }
 
-            $('.modal-bg').toggle();
-            $('.modal').toggle();
+//Populates the modal with information and toggles it.
+    function toggleDetails() {
+
+        $('.modal-bg').toggle();
+        $('.modal').toggle();
+
+        var currentPokemon = pokeHelper($(this).attr("id"));
+
+        //only do all this stuff if we're displaying the modal, not hiding it
+
+        if ($('.modal').css("display") === "none") {
+
+            //turn on scrolling
+            $("html, body").css("overflow","auto");
+
+            //remove elements from type + evolution
+            $('.evolution-item').remove();
+            $('.type-item').remove();
+
+        }
+        else {
+
+            //populate the pokemon's name
+            $(".modal-pokename").text(currentPokemon.name);
+
+            //turn off scrolling
+            $("html, body").css("overflow","hidden");
+
+            $(".modal-pokeimage").attr("src",currentPokemon.image_url);
 
             //appends correct information to "types" section
-            $('.modal-typelist').append(listMaker(currentPokemon.types));
-
-            //inserts correct evolution info into table if not empty. If empty, hides table.
-            
+            $('.modal-typelist').append(listMaker(currentPokemon.types));    
 
             //creates the table for evolutions for a given pokemon, or hides the table altogether
-            var tableString = "";
             if (currentPokemon.evolutions.length != 0) {
                 $('.modal-evolution-table').append(tableRowMaker(currentPokemon.evolutions));
             }
             else {
                 $('.modal-evolution-table').hide();
             }
-
-
-            $(this).find('.pokedetails').toggle();
-            $(".modal-bg").toggle();
-
-            if ($(this).css("height") === "200px") {
-
-                //turn the pokemon element into a modal
-                $(this)
-                    .off("mouseenter");
-
-                    
-                //turn off scrolling
-                $("html, body").css("overflow","hidden");
-
-            }
-            else {
-
-                //return the element to normal
-                $(this)
-                    .on("mouseenter", this, pokehoverEnter);
-
-            
-                //turn on scrolling
-                $("html, body").css("overflow","auto");
-
-            }
             
         }
+        
+    }
 
-
-    
-
-    });
-    
+});
